28aeb13edb7226bf2d3805b64559d01e8ebea920
unknown <cjj9327@naver.com>
2019-05-03 13:06:31 +0900

    there is a little bit of revision

--- socket_server.py
+++ socket_server.py
@@ -1,4 +1,5 @@
 import socket
+import struct
 
 SERVER_IP_ADDRESS = "192.168.0.8"
 SERVER_PORT_NUM = 56774
@@ -6,13 +7,15 @@ PI_IP_ADDRESS = "192.168.137.179"
 PI_PORT_NUM = 43179
 
 if __name__ == "__main__":
-    server_socket = socket.socket(socket_AF_INET, socket.SOCK_STREAM)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.connect(PI_IP_ADDRESS, PI_PORT_NUM)
 
     while True:
         recv_msg = input('What is the value u want?')
+        recv_int = int(recv_msg)
+        data = [recv_int]
         data_format = struct.Struct("i")
-        pwm_value = data_format.pack(*recv_msg)
+        pwm_value = data_format.pack(*data)
         server_socket.send(pwm_value)
         server_socket.close()
 
