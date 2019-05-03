04edc36653f3febe4e294d795b00c8cb3f89fd68
unknown <cjj9327@naver.com>
2019-05-03 16:15:33 +0900

    plus threading

--- socket_pi.py
+++ socket_pi.py
@@ -1,23 +1,33 @@
 import socket
+import struct
+import threading
 
 SERVER_IP_ADDRESS = "192.168.0.8"
 SERVER_PORT_NUM = 56774
-PI_IP_ADDRESS = "192.168.137.179"
+PI_IP_ADDRESS = "192.168.0.2"
 PI_PORT_NUM = 43179
 
-if __name__ == "__main__": 
-    clnt_socket = socket.socket(socket.AF_INET, socket.socket.SOCK_STREAM)
-    clnt_socket.bind((PI_IP_ADDRESS, PI_PIRT_NUM))
-    clnt_socket.listen()
-
+def detected_sign_from_server(self):
     while True:
+        clnt_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        clnt_socket.bind((PI_IP_ADDRESS, PI_PORT_NUM))
+        clnt_socket.listen()    
         conn, addr = clnt_socket.accept()
         temp_msg = conn.recv(1024)
         data_format = struct.Struct("i")
         recv_value = data_format.unpack(temp_msg)
+        recv_int = recv_value[0]
 
-        print("received msg: ", temp_msg)
+        print("received msg: ", recv_int)
+
+        ''' Implement some codes for recv sign '''
+
         conn.close()
+
+
+if __name__ == "__main__": 
+    sign_detection_thread = threading.Thread(target=detected_sign_from_server)
+    sign_detection_thread.start()
 
 
 
--- socket_server.py
+++ socket_server.py
@@ -6,10 +6,7 @@ SERVER_PORT_NUM = 56774
 PI_IP_ADDRESS = "192.168.137.179"
 PI_PORT_NUM = 43179
 
-if __name__ == "__main__":
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.connect(PI_IP_ADDRESS, PI_PORT_NUM)
-
+def send_detected_sign_to_pi(self):
     while True:
         recv_msg = input('What is the value u want?')
         recv_int = int(recv_msg)
@@ -19,6 +16,13 @@ if __name__ == "__main__":
         server_socket.send(pwm_value)
         server_socket.close()
 
+
+if __name__ == "__main__":
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.connect(PI_IP_ADDRESS, PI_PORT_NUM)
+    
+    send_detected_sign_to_pi()
+    
 
 
 
